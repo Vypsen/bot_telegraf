797a4be9ff02aa023a35867a80e2935de2c09018
Sergei Semenets <s.semenets2001@mail.ru>
2022-02-13 02:01:39 +1000

    fix bug check date

--- reminder-scene.js
+++ reminder-scene.js
@@ -10,7 +10,7 @@ const reminderScene = new BaseScene('reminderScene')
 const date_pattern = /\d{1,2}.\d{1,2}.\d{2,4} \d{1,2}:\d{2}/            //pattern for date 
 
 reminderScene.enter(ctx =>{ctx.reply(
-    'message format: "event dd.mm.yy hh.mm.ss"',
+    'message format: "event dd.mm.yy hh:mm"',
     exit_keyboard)    
 })
 
@@ -23,17 +23,19 @@ reminderScene.on('text', async ctx=>{
     var user_message = ctx.message.text
     const arr_user_message = user_message.split(" ")
 
-    var year = arr_user_message[arr_user_message.length-2].split('.')   // get dd.mm.yy
+    var year = arr_user_message[arr_user_message.length-2] // get dd.mm.yy
     var time = arr_user_message[arr_user_message.length-1]              //get time
 
-    var reminer_time = year.join('.') + ' ' + time    //year + time
-    const date = new Date(`${year[1]}.${year[0]}.${year[2]} ${time}`)   //date in the format mm.dd.yy
+    var reminer_time = year + ' ' + time    //year + time
 
-    if(arr_user_message.length < 3 || !date_pattern.test(reminer_time) || date == 'Invalid Date') //check message format 
+    if(arr_user_message.length < 3 || !date_pattern.test(reminer_time) ) //check message format 
     {
         return ctx.reply('invalid input')
     }
+    var year = year.split('.')   // get dd.mm.yy
 
+    const date = new Date(`${year[1]}.${year[0]}.${year[2]} ${time}`)   //date in the format mm.dd.yy
+    if(date == 'Invalid Date')return ctx.reply('invalid input')
     var user_message_without_time  = '' 
     for (i = 0; i < arr_user_message.length-2; i++) {               //get event from message
         user_message_without_time +=  arr_user_message[i] + ' '
